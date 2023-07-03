e1e70cfa2908dc65850509f0e178d3ea3b57c4c2
Gigz <gigzpumpking@gmail.com>
2023-07-03 00:42:13 -0700

    Loading more assets

--- src/scenes/Load.js
+++ src/scenes/Load.js
@@ -18,45 +18,59 @@ class Load extends Phaser.Scene {
             loadingBar.destroy();
         });
 
-        this.load.path = './assets/';
-
         // Title Graphical Assets
-
-        this.load.image('titleText', 'images/UI/title_text.png');
-        this.load.image('creditsButton', 'images/UI/credits_button.png');
-        this.load.image('tutorialButton', 'images/UI/how_to_play_button.png');
-        this.load.image('optionsButton', 'images/UI/options_button.png');
-        this.load.image('startButton', 'images/UI/start_button.png');
+        this.load.path = './assets/images/UI/';
+        this.load.image('titleText', 'title_text.png');
+        this.load.image('creditsButton', 'credits_button.png');
+        this.load.image('tutorialButton', 'how_to_play_button.png');
+        this.load.image('optionsButton', 'options_button.png');
+        this.load.image('startButton', 'start_button.png');
 
         // Play Scene Graphical Assets
+        this.load.path = './assets/';
         this.load.image('Silhouette', 'silhouette_sprites/silhouette_3.png');
+        this.load.image('SilhouetteUP', 'silhouette_sprites/silhouette_lookingup.png');
+        this.load.image('SilhouetteSTARE', 'silhouette_sprites/silhouette_stare.png');
         this.load.spritesheet('SilhouetteAnims', 'anims/silhouette_animation5-Sheet.png', {frameWidth: 23, frameHeight: 69, startFrame: 0, endFrame: 6});
         this.load.spritesheet('SmokingCigAnims', 'anims/smoking_ciggy-Sheet.png', {frameWidth: 192, frameHeight: 108, startFrame: 0, endFrame: 3});
 
         // Trash Basket and Paper Ball Graphical Assets
-        this.load.image('Basket0', 'images/trash_basket/empty_basket.png')
-        this.load.image('Basket1', 'images/trash_basket/basket_filling_1.png');
-        this.load.image('Basket2', 'images/trash_basket/basket_filling_2.png');
-        this.load.image('Basket3', 'images/trash_basket/basket_filling_3.png');
-        this.load.image('Basket4', 'images/trash_basket/basket_filling_4.png');
-        this.load.image('paperball', 'images/trash_basket/paper_ball.png');
-        this.load.image('fireBasketIdle', 'images/trash_basket/FireSpriteSheets/basket_fire_idle.png');
-        this.load.image('fireBasketStart', 'images/trash_basket/FireSpriteSheets/basket_fire_start.png');
-        this.load.image('fireBallIdle', 'images/trash_basket/FireSpriteSheets/flaming_ball_idle.png');
-        this.load.image('fireBallThrown', 'images/trash_basket/FireSpriteSheets/flaming_ball_thrown.png');
+        this.load.path = './assets/images/trash_basket/';
+        this.load.image('Basket0', 'empty_basket.png')
+        this.load.image('Basket1', 'basket_filling_1.png');
+        this.load.image('Basket2', 'basket_filling_2.png');
+        this.load.image('Basket3', 'basket_filling_3.png');
+        this.load.image('Basket4', 'basket_filling_4.png');
+        this.load.image('paperball', 'paper_ball.png');
+        this.load.image('fireBasketIdle', 'FireSpriteSheets/basket_fire_idle.png');
+        this.load.image('fireBasketStart', 'FireSpriteSheets/basket_fire_start.png');
+        this.load.image('fireBallIdle', 'FireSpriteSheets/flaming_ball_idle.png');
+        this.load.image('fireBallThrown', 'FireSpriteSheets/flaming_ball_thrown.png');
 
         // Desk Graphical Assets
-        this.load.image('ashtray0', 'desk_items/ashtray_0.png');
-        this.load.image('ashtray1', 'desk_items/ashtray_1.png');
-        this.load.image('ashtray2', 'desk_items/ashtray_2.png');
-        this.load.image('ashtray3', 'desk_items/ashtray_3.png');
-        this.load.image('cigbox', 'desk_items/cigbox.png');
-        this.load.image('keyboard', 'desk_items/keyboardV4.png');
+        this.load.path = './assets/desk_items/';
+        this.load.image('ashtray0', 'ashtray_0.png');
+        this.load.image('ashtray1', 'ashtray_1.png');
+        this.load.image('ashtray2', 'ashtray_2.png');
+        this.load.image('ashtray3', 'ashtray_3.png');
+        this.load.image('cigbox', 'cigbox.png');
+        this.load.image('keyboard', 'keyboardV4.png');
 
         // Computer Graphical Assets
-        this.load.image('computer', 'computer/computer_background.png');
-        this.load.image('mail', 'computer/mail_title.png');
-        this.load.image('inbox', 'computer/inbox_screen.png');
+        this.load.path = './assets/computer/';
+        this.load.image('computer', 'computer_background.png');
+        this.load.image('mail', 'mail_title.png');
+        this.load.image('inbox', 'inbox_screen.png');
+
+        // Paper Sorting Graphical Assets
+        this.load.path = './assets/paper_sorting_game/';
+        this.load.image('leftTray', 'left_tray.png');
+        this.load.image('rightTray', 'right_tray.png');
+        this.load.image('paperSortBG', 'paper_sort_bg.png');
+        this.load.image('paperStackA', 'paper_stack_A.png');
+        this.load.image('paperStackB', 'paper_stack_B.png');
+        this.load.image('sortText', 'sort_text.png');
+
     }
 
     create() {
